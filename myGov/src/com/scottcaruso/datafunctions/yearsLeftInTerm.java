97aaa184951c2a45e86b9a80d723dfc68b49a5a8
Scott Caruso <scottcaruso@fullsail.edu>
2013-08-01 06:50:45 -0400

    Created ENUM to use. Just need to hook it up.

---
+++ myGov/src/com/scottcaruso/datafunctions/yearsLeftInTerm.java
@@ -0,0 +1,31 @@
+package com.scottcaruso.datafunctions;
+
+public enum yearsLeftInTerm {
+	
+	REPRESENTATIVE(2),
+	SENATOR(6);
+	
+	private final int numberOfYears;
+	
+	yearsLeftInTerm(int value)
+	{
+		this.numberOfYears = value;
+	}
+	
+	public static int yearsLeft(int yearTookOffice, int currentYear, String role)
+	{
+		if (role.equals("Representative"))
+		{
+			int yearsServed = currentYear - yearTookOffice;
+			int yearsLeft = REPRESENTATIVE.numberOfYears - yearsServed;
+			return yearsLeft;
+		}
+		if (role.equals("Senator"))
+		{
+			int yearsServed = currentYear - yearTookOffice;
+			int yearsLeft = SENATOR.numberOfYears - yearsServed;
+			return yearsLeft;
+		}
+		return 0;
+	}
+}
